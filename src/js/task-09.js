d4758bf889ae94b453d137c8df00aba8c523f65e
Sarat862 <sarat862@gmail.com>
2022-07-12 00:47:34 +0300

    Виконав 10 завдання

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,6 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+console.log(getRandomHexColor());
 
 const buttonEl = document.querySelector('.change-color');
 const body = document.querySelector('body');
@@ -12,3 +13,14 @@ function onButtonClick() {
   body.style.backgroundColor = getRandomHexColor();
   textEl.textContent = getRandomHexColor();
 }
+
+// function componentToHex(c) {
+//   var hex = c.toString(16);
+//   return hex.length == 1 ? "0" + hex : hex;
+// }
+
+// function rgbToHex(r, g, b) {
+//   return `#${componentToHex(r)}${componentToHex(g)}${componentToHex(b)}`
+// }
+
+// alert(rgbToHex(0, 51, 255));
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector('input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+let sizeBox = 30;
+
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = inputEl.value;
+  for (let i = 0; i < amount; i += 1) {
+    const createBox = document.createElement('div');
+    boxes.append(createBox);
+    createBox.style.width = `${sizeBox}px`;
+    createBox.style.height = `${sizeBox}px`;
+    createBox.style.backgroundColor = getRandomHexColor();
+    sizeBox += 10;
+  } 
+}
+
+function destroyBoxes() {
+  while (boxes.firstElementChild) {
+    boxes.firstElementChild.remove()
+  }
+  inputEl.value = '';
+}
